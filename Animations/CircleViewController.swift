a78dc4ac93c397e2bf9694b76f45fab77bacb300
Huang Rui <richard34huang@hotmail.com>
2017-08-25 14:48:32 +0800

    commit

---
+++ Animations/CircleViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CircleViewController.swift
+//  Animations
+//
+//  Created by 黄瑞 on 2017/8/25.
+//  Copyright © 2017年 黄瑞. All rights reserved.
+//
+
+import UIKit
+
+class CircleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
